b020154ee20170553b3f49b774bab9f3ec2bdc60
Adrien Crivelli <adrien.crivelli@gmail.com>
2024-09-06 21:04:44 +0200

    naturalBackgroundDensity should never be i18n-ed

--- projects/natural/src/lib/modules/common/directives/background-density.directive.spec.ts
+++ projects/natural/src/lib/modules/common/directives/background-density.directive.spec.ts
@@ -6,19 +6,19 @@ import {NaturalBackgroundDensityDirective} from './background-density.directive'
 @Component({
     template: `
         <div naturalBackgroundDensity></div>
-        <div naturalBackgroundDensity="https://example.com/api/image/123/200" i18n-naturalBackgroundDensity></div>
+        <div naturalBackgroundDensity="https://example.com/api/image/123/200"></div>
         <div [naturalBackgroundDensity]="'https://example.com/api/image/123/200'"></div>
         <div [naturalBackgroundDensity]="'https://example.com/api/image/123.jpg'"></div>
         <div
             style="color: red; background-image: url('foo.jpg');"
-            naturalBackgroundDensity="https://example.com/api/image/123/200" i18n-naturalBackgroundDensity
+            naturalBackgroundDensity="https://example.com/api/image/123/200"
         ></div>
         <div
             style="color: red; background-image: url('foo.jpg');"
-            naturalBackgroundDensity="https://example.com/api/image/123.jpg" i18n-naturalBackgroundDensity
+            naturalBackgroundDensity="https://example.com/api/image/123.jpg"
         ></div>
-        <div naturalBackgroundDensity="https://example.com/api/image/123/201" i18n-naturalBackgroundDensity></div>
-        <div naturalBackgroundDensity="url(data:image/png;base64,aabbcc)" i18n-naturalBackgroundDensity></div>
+        <div naturalBackgroundDensity="https://example.com/api/image/123/201"></div>
+        <div naturalBackgroundDensity="url(data:image/png;base64,aabbcc)"></div>
     `,
     standalone: true,
     imports: [NaturalBackgroundDensityDirective],
