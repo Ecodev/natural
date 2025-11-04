4aaa439444bb0fee53b033fd59107477b33e7611
Samuel Baptista <samuel.baptista@ecodev.ch>
2025-11-04 01:50:00 +0100

    Fix tests

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,7 +4,7 @@ import {MatIcon} from '@angular/material/icon';
 import {MatListItem, MatListItemIcon, MatNavList} from '@angular/material/list';
 import {MatToolbar} from '@angular/material/toolbar';
 import {Router, RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
-import {NaturalColorSchemerComponent, NaturalThemeChangerComponent, NaturalThemeService} from '@ecodev/natural';
+import {NaturalColorSchemerComponent, NaturalThemeChangerComponent} from '@ecodev/natural';
 import {NaturalIconDirective} from '../../../projects/natural/src/lib/modules/icon/icon.directive';
 import {NaturalSidenavContainerComponent} from '../../../projects/natural/src/lib/modules/sidenav/sidenav-container/sidenav-container.component';
 import {NaturalSidenavContentComponent} from '../../../projects/natural/src/lib/modules/sidenav/sidenav-content/sidenav-content.component';
@@ -34,7 +34,6 @@ import {NaturalSidenavComponent} from '../../../projects/natural/src/lib/modules
     styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-    public readonly themeService = inject(NaturalThemeService);
     public readonly router = inject(Router);
 
     protected isActive(url: string): boolean {
