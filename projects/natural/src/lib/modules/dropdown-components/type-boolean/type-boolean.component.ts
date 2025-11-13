0a4379b5e734bb13180106240407d023fe11718b
Adrien Crivelli <adrien.crivelli@gmail.com>
2025-11-13 20:06:17 +0900

    Drop obsolete experimentalDecorators #11918
    Because we don't use parameter decorators anymore, so we don't need it.

--- projects/natural/src/lib/modules/dropdown-components/type-boolean/type-boolean.component.ts
+++ projects/natural/src/lib/modules/dropdown-components/type-boolean/type-boolean.component.ts
@@ -13,30 +13,32 @@ export type TypeBooleanConfiguration = {
 @Component({
     imports: [FormsModule, ReactiveFormsModule, MatButtonToggleGroup, MatButtonToggle],
     templateUrl: '../type-options/type-options.component.html',
+    providers: [
+        {
+            provide: NATURAL_DROPDOWN_DATA,
+            useFactory: (): NaturalDropdownData<TypeOptionsConfiguration> => {
+                const data = inject<NaturalDropdownData<TypeBooleanConfiguration>>(NATURAL_DROPDOWN_DATA, {
+                    skipSelf: true,
+                });
+
+                // Set up options from hardcoded conditions with custom labels from config
+                return {
+                    ...data,
+                    configuration: {
+                        options: [
+                            {
+                                display: data.configuration.displayWhenActive,
+                                condition: {equal: {value: true}},
+                            },
+                            {
+                                display: data.configuration.displayWhenInactive,
+                                condition: {equal: {value: false}},
+                            },
+                        ],
+                    },
+                };
+            },
+        },
+    ],
 })
-export class TypeBooleanComponent extends TypeOptionsComponent implements DropdownComponent {
-    public constructor() {
-        const data = inject<NaturalDropdownData<TypeBooleanConfiguration>>(NATURAL_DROPDOWN_DATA);
-
-        // Set up options from hardcoded conditions with custom labels from config
-        const configuration: TypeOptionsConfiguration = {
-            options: [
-                {
-                    display: data.configuration.displayWhenActive,
-                    condition: {equal: {value: true}},
-                },
-                {
-                    display: data.configuration.displayWhenInactive,
-                    condition: {equal: {value: false}},
-                },
-            ],
-        };
-
-        const typeOptionsData: NaturalDropdownData<TypeOptionsConfiguration> = {
-            ...data,
-            ...{configuration: configuration},
-        };
-
-        super(typeOptionsData);
-    }
-}
+export class TypeBooleanComponent extends TypeOptionsComponent implements DropdownComponent {}
