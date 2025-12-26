a3e8992d4574bfa476ea3aab96623e750109ef07
Adrien Crivelli <adrien.crivelli@gmail.com>
2025-12-26 14:47:51 +0900

    Tahiti has half hour timezones

--- projects/natural/src/lib/classes/utility.ts
+++ projects/natural/src/lib/classes/utility.ts
@@ -36,7 +36,7 @@ export function formatIsoDateTime(date: Date): string {
     const timezoneOffsetInMinutes = Math.round(date.getTimezoneOffset());
     const timezoneOffsetInHours = -Math.trunc(timezoneOffsetInMinutes / 60); // UTC minus local time
     const sign = timezoneOffsetInHours >= 0 ? '+' : '-';
-    const remainderMinutes = -(timezoneOffsetInMinutes % 60);
+    const remainderMinutes = timezoneOffsetInMinutes % 60;
 
     // It's a bit unfortunate that we need to construct a new Date instance,
     // but we don't want the original Date instance to be modified
@@ -57,7 +57,7 @@ export function formatIsoDateTime(date: Date): string {
         .replace('Z', '');
 
     const hours = Math.abs(timezoneOffsetInHours).toString().padStart(2, '0');
-    const minutes = remainderMinutes.toString().padStart(2, '0');
+    const minutes = Math.abs(remainderMinutes).toString().padStart(2, '0');
 
     return iso + sign + hours + ':' + minutes;
 }
