fecf731c9217f8226a514633455de64c2a4c6484
Samuel Baptista <samuel.baptista@ecodev.ch>
2026-02-12 00:19:04 +0100

    Preserve default FAB button as primary, and default natural-fixed-button as tertiary #11936

--- src/app/buttons/buttons.component.ts
+++ src/app/buttons/buttons.component.ts
@@ -1,13 +1,29 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 import {MatButton, MatFabButton, MatIconButton, MatMiniFabButton} from '@angular/material/button';
 import {MatIcon} from '@angular/material/icon';
-import {NaturalIconDirective} from '@ecodev/natural';
+import {MatSlideToggle} from '@angular/material/slide-toggle';
+import {MatTooltip} from '@angular/material/tooltip';
+import {NaturalFixedButtonComponent, NaturalIconDirective} from '@ecodev/natural';
 
 @Component({
     selector: 'app-buttons',
-    imports: [NaturalIconDirective, MatButton, MatMiniFabButton, MatFabButton, MatIconButton, MatIcon],
+    imports: [
+        NaturalIconDirective,
+        MatButton,
+        MatMiniFabButton,
+        MatFabButton,
+        MatIconButton,
+        MatIcon,
+        NaturalFixedButtonComponent,
+        FormsModule,
+        MatSlideToggle,
+        MatTooltip,
+    ],
     templateUrl: './buttons.component.html',
     styleUrl: './buttons.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ButtonsComponent {}
+export class ButtonsComponent {
+    public disabled = false;
+}
