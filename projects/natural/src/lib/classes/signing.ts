66c1c4314e561143c56a0b86690b86f8ad0a01c4
Adrien Crivelli <adrien.crivelli@gmail.com>
2026-02-12 16:39:06 +0900

    Avoid sensitive terms in compiled source #12190
    Error messages might give hint on how to hack us, so we obfuscate them.

--- projects/natural/src/lib/classes/signing.ts
+++ projects/natural/src/lib/classes/signing.ts
@@ -2,13 +2,20 @@ import {HttpInterceptorFn, HttpRequest} from '@angular/common/http';
 import {hmacSha256} from './crypto';
 import {map, of, switchMap, throwError} from 'rxjs';
 
+// Keep those strings obfuscated, to make it harder to CTRL+F things in compiled code
+const cannotSignAGraphQLQueryThatIsUsingFormDataButThatIsMissingTheKeyOperations = atob(
+    'Q2Fubm90IHNpZ24gYSBHcmFwaFFMIHF1ZXJ5IHRoYXQgaXMgdXNpbmcgRm9ybURhdGEgYnV0IHRoYXQgaXMgbWlzc2luZyB0aGUga2V5IGBvcGVyYXRpb25zYA==',
+);
+const graphqlQuerySignerRequiresANonEmptyKeyConfigureItInLocalPphpUnderSignedQueries = atob(
+    'Z3JhcGhxbFF1ZXJ5U2lnbmVyIHJlcXVpcmVzIGEgbm9uLWVtcHR5IGtleS4gQ29uZmlndXJlIGl0IGluIGxvY2FsLnBocCB1bmRlciBzaWduZWRRdWVyaWVzLg==',
+);
+const xSignature = atob('WC1TaWduYXR1cmU=');
+
 function getOperations(req: HttpRequest<unknown>): string {
     if (req.body instanceof FormData) {
         const operations = req.body.get('operations');
         if (typeof operations !== 'string') {
-            throw new Error(
-                'Cannot sign a GraphQL query that is using FormData but that is missing the key `operations`',
-            );
+            throw new Error(cannotSignAGraphQLQueryThatIsUsingFormDataButThatIsMissingTheKeyOperations);
         }
         return operations;
     } else {
@@ -25,13 +32,11 @@ export function graphqlQuerySigner(key: string): HttpInterceptorFn {
     // Validates the configuration exactly 1 time (not for
     // every query), and if not reject **all** HTTP requests
     if (!key) {
-        return () =>
-            throwError(
-                () =>
-                    new Error(
-                        'graphqlQuerySigner requires a non-empty key. Configure it in local.php under signedQueries.',
-                    ),
+        return () => {
+            return throwError(
+                () => new Error(graphqlQuerySignerRequiresANonEmptyKeyConfigureItInLocalPphpUnderSignedQueries),
             );
+        };
     }
 
     return (req, next) => {
@@ -60,7 +65,7 @@ export function graphqlQuerySigner(key: string): HttpInterceptorFn {
             switchMap(data => {
                 const header = `v1.${data.timestamp}.${data.hash}`;
                 const signedRequest = req.clone({
-                    headers: req.headers.set('X-Signature', header),
+                    headers: req.headers.set(xSignature, header),
                 });
 
                 return next(signedRequest);
