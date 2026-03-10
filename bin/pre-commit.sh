bbfb23337a4216b30d2412991f54e5f0550a698e
Adrien Crivelli <adrien.crivelli@gmail.com>
2026-03-10 22:51:56 +0900

    Correct comment #12402

--- bin/pre-commit.sh
+++ bin/pre-commit.sh
@@ -31,7 +31,7 @@ fi
 files=$(git diff --cached --name-only --diff-filter=ACMR | grep -E '\.(html|xlf)$')
 if [ "$files" != "" ]; then
 
-    # Run eslint before commit
+    # Run i18n checks before commit
     echo "$files" | xargs | xargs ./projects/natural/bin/i18n-check.ts
     if [ $? -ne 0 ]; then
         pass=false
