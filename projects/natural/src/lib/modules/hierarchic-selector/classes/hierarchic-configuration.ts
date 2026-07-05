913e7a12dca432a9040cfaf0bd5b5710d6b0f798
Samuel Baptista <sam.bapt@proton.me>
2026-07-06 00:37:19 +0200

    Type hierarchic output

--- projects/natural/src/lib/modules/hierarchic-selector/classes/hierarchic-configuration.ts
+++ projects/natural/src/lib/modules/hierarchic-selector/classes/hierarchic-configuration.ts
@@ -1,26 +1,34 @@
 import {Type} from '@angular/core';
-import {type ExtractTallOne, type ExtractVall, UntypedModelService} from '../../../types/types';
+import {PaginatedData} from '../../../classes/data-source';
+import {QueryVariables} from '../../../classes/query-variable-manager';
+import {NaturalAbstractModelService} from '../../../services/abstract-model.service';
+import {type ExtractTallOne, type ExtractVall, Literal} from '../../../types/types';
 
-export type NodeConfig<T extends UntypedModelService = UntypedModelService> = {
+// More globally in types.ts ?
+export type ListableModelService = NaturalAbstractModelService<
+    any,
+    any,
+    PaginatedData<Literal>,
+    QueryVariables,
+    any,
+    any,
+    any,
+    any,
+    any,
+    any
+>;
+
+// Remove = ListableModelService ? keep the extends part only ?
+export type NodeConfig<T extends ListableModelService = ListableModelService> = {
     /**
      * An AbstractModelService to be used to fetch items
      */
     service: Type<T>;
 
     /**
-     * Whether this node is at the root of the tree (there can be multiple roots in one tree)
-     */
-    root?: boolean;
-
-    /**
      * Additional filters applied in the query sent by getList function
      */
     filter?: ExtractVall<T>['filter'];
-
-    /**
-     * Key of the returned literal container models by config / service
-     */
-    selectableAtKey?: string;
 
     /**
      * Displayed icon for items retrieved for that config
@@ -68,25 +76,72 @@ type RelationConfig<Nodes extends NodeConfig[]> = {
     field: string;
 };
 
-export type NaturalHierarchicConfiguration<Nodes extends NodeConfig[]> = {
+export type NaturalHierarchicConfiguration<
+    Node extends NodeConfig[] = NodeConfig[],
+    Selectables extends OrganizedSelectableConfigs<Node> = OrganizedSelectableConfigs<Node>,
+> = {
     /**
      * All possible nodes in the tree
      */
-    nodes: Nodes;
+    nodes: Node;
 
     /**
      * All possible relations between nodes
      */
-    relations: RelationConfig<Nodes>[];
+    relations: RelationConfig<Node>[];
+
+    /**
+     * List of nodes used for the root of the tree
+     */
+    roots: Node[number][];
+
+    /**
+     * List of nodes for selectable elements, organized by key.
+     */
+    selectables: Selectables;
 };
 
-export function nodeConfig<T extends UntypedModelService>(node: NodeConfig<T>): NodeConfig<T> {
+export function nodeConfig<T extends ListableModelService>(node: NodeConfig<T>): NodeConfig<T> {
     return node;
 }
 
-export function hierarchicConfig<Nodes extends NodeConfig[]>(
+export function hierarchicConfig<
+    const Nodes extends NodeConfig[],
+    const Selectables extends OrganizedSelectableConfigs<Nodes>,
+>(
     nodes: Nodes,
+    roots: Nodes[number][],
+    selectables: Selectables,
     relations: RelationConfig<Nodes>[],
-): NaturalHierarchicConfiguration<Nodes> {
-    return {nodes: nodes, relations: relations};
+): NaturalHierarchicConfiguration<Nodes, Selectables> {
+    return {nodes, roots, selectables, relations};
 }
+
+// Outputs / Resulting selections
+
+/**
+ * Equivalent to `OrganizedSelections` but for the config (input) side.
+ * List of selectable nodes (services) organized by key.
+ */
+export type OrganizedSelectableConfigs<Nodes extends NodeConfig[]> = Record<string, Nodes[number][]>;
+
+/**
+ * The model type of a single node config, eg: NodeConfig<QuestionService> → Question
+ */
+type Model<SingleNodeConfig extends NodeConfig> = SingleNodeConfig extends NodeConfig<infer T>
+    ? ExtractTallOne<T>
+    : never;
+
+/**
+ * The models selected for a single selectable slot (one key of `selectables`)
+ */
+export type Selections<Nodes extends NodeConfig[]> = Model<Nodes[number]>[];
+
+/**
+ * All selections, organized by key, typed from the configuration's `selectables`.
+ *
+ * Replaces the old `OrganizedModelSelection = Record<string, any[]>`.
+ */
+export type OrganizedSelections<Selectables extends OrganizedSelectableConfigs<any> = OrganizedSelectableConfigs<any>> = {
+    [Key in keyof Selectables]: Selections<Selectables[Key]>;
+};
