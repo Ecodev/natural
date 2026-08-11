2c6dcc4ba608c0e7aa8aac070ea64aa5be33516a
Adrien Crivelli <adrien.crivelli@gmail.com>
2026-08-11 11:09:47 +0900

    Drop obsolete environnement #12539

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import {provideHttpClient} from '@angular/common/http';
-import {enableProdMode, provideZoneChangeDetection} from '@angular/core';
+import {provideZoneChangeDetection} from '@angular/core';
 import {DateAdapter, provideNativeDateAdapter} from '@angular/material/core';
 import {MAT_PAGINATOR_DEFAULT_OPTIONS, type MatPaginatorDefaultOptions} from '@angular/material/paginator';
 import {MAT_TABS_CONFIG, type MatTabsConfig} from '@angular/material/tabs';
@@ -19,13 +19,8 @@ import {routes} from './app/app.routes';
 import {AppComponent} from './app/app.component';
 import {DemoLoggerExtra} from './app/demo.error-handler';
 import {AnyLinkMutationService} from './app/shared/services/any-link-mutation.service';
-import {environment} from './environments/environment';
 import {provideApollo} from 'apollo-angular';
 import {apolloOptionsFactory} from './app/shared/config/apollo-options.provider';
-
-if (environment.production) {
-    enableProdMode();
-}
 
 bootstrapApplication(AppComponent, {
     providers: [
